a8dfd529b4ff78848a19622db8796c62a88e8213
User <uriyderechey@gmail.com>
2021-12-20 09:52:13 +0200

    add grid component

--- markup/static/js/custom.js
+++ markup/static/js/custom.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
+    var $burger = $('.js-header-burger');
+    var $nav = $('.js-header-toggle nav');
+    $burger.on('click', function () {
+        $(this).toggleClass('open');
+        $('.header').toggleClass('open');
+        $nav.toggleClass('open');
+        $('body').toggleClass('fixed-position');
+    });
+
+    $(window).scroll(function () {
+        var height = $(window).scrollTop();
+
+        if (height > 50) {
+            $('.js-header').addClass('is-scroll');
+        } else {
+            $('.js-header').removeClass('is-scroll');
+        }
+    });
+});
+
+$(document).ready(function () {
     //add animation BODY
     $('body').AddClassAnimation();
 
