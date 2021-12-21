b8d5f6bef386073cf907267d26f72af79004c1d5
User <uriyderechey@gmail.com>
2021-12-21 15:36:36 +0200

    add new form

--- markup/static/js/custom.js
+++ markup/static/js/custom.js
@@ -17,6 +17,252 @@ $(document).ready(function () {
             $('.js-header').removeClass('is-scroll');
         }
     });
+
+
+
+    //form
+    var FormValidator = (function($, window){
+
+        'use strict';
+
+        function FormValidator(formSelector) {
+            this.$form = $(formSelector);
+            this.errorMsgs = [];
+            this.fieldArray = [];
+        }
+
+        /**
+         Attributes:
+         @selector (required)
+         @name (optional)
+         @required (optional, default is false)
+         @match (optional, regex)
+         @sameAs (optional, a field to be the same)
+         @error (optional, error callback function)
+         @success (optional, success callback function)
+         **/
+
+        FormValidator.prototype.config = function(fieldArray) {
+            if( Object.prototype.toString.call( fieldArray ) !== '[object Array]' ) {
+                /*throw new Error('Parameters must be array');*/
+                return;
+            }
+            this.fieldArray = fieldArray;
+        }
+
+
+        FormValidator.prototype.submit = function() {
+
+            this.errorMsgs.length = 0;
+
+            for(var i=0; i<this.fieldArray.length; i++) {
+
+                var field = this.fieldArray[i];
+                if( Object.prototype.toString.call( field ) !== '[object Object]' ) {
+                    throw new Error('Each field must be object');
+                    return;
+                }
+
+                field.error = field.error || function(){};
+                field.success = field.success || function(){};
+
+                if ( !field.selector ) {
+                    throw new Error('Attribute selector is required');
+                    return;
+                }
+                field.$element = this.$form.find($(field.selector));
+
+                if (field.$element.length > 0) {
+                    if (!this.isRequiredValid(field)) {
+                        //invalid
+                        this.errorMsgs.push({name: field.name, msg: this.generateMessage('required', field.name), attribute: 'required'});
+                        continue;
+                    };
+
+                    if (!this.isMatchValid(field)) {
+                        //invalid
+                        this.errorMsgs.push({name: field.name, msg: this.generateMessage('match', field.name), attribute: 'match'});
+                        continue;
+                    };
+
+                    if (!this.isTheSame(field)) {
+                        //invalid
+                        this.errorMsgs.push({name: field.name, msg: this.generateMessage('sameAs', field.name), attribute: 'sameAs'});
+                        continue;
+                    };
+                    // console.log(field);
+                    field.success.call(field);
+
+                }
+            }
+
+            if(this.errorMsgs.length > 0)
+                return false;
+
+            return true;
+
+        }
+
+
+        FormValidator.prototype.isRequiredValid = function(field) {
+
+            var isRequired = field.required || false;
+            if (!isRequired)
+                return true;
+
+            if (field.$element.val()) {
+                return true;
+            } else {
+                //execute error function
+                field.error.call(field);
+                return false;
+            }
+
+        }
+
+        FormValidator.prototype.isMatchValid = function(field) {
+
+            var regex = field.match || false;
+            if (!regex)
+                return true;
+
+            if (field.$element.val().match(regex)) {
+                return true;
+            } else {
+                field.error.call(field);
+                return false;
+            }
+
+        }
+
+        FormValidator.prototype.isTheSame = function(field) {
+            var $anotherEle = this.$form.find($(field.sameAs));
+            if ($anotherEle.length == 0)
+                return true;
+
+            if (field.$element.val() == $anotherEle.val()) {
+                return true;
+            } else {
+                field.error.call(field);
+                return false;
+            }
+        }
+
+
+        FormValidator.prototype.generateMessage = function(attr, name) {
+
+            name = name || 'This field';
+            var msg = '';
+            switch(attr) {
+
+                case 'required':
+                    msg = name + ' is required';
+                    break;
+                case 'match':
+                    msg = name + ' doesn\'t match the pattern';
+                    break;
+                case 'sameAs':
+                    msg = name + ' should be the same';
+                    break;
+
+                default:
+                    break;
+
+            }
+            return msg;
+        }
+        return FormValidator;
+    })(jQuery, window);
+
+
+
+// load
+    $(document).ready(function(){
+
+        //register form validation
+        window.validator = new FormValidator('.register-form');
+        validator.config([
+            //first name
+            {
+                selector: '[name="name"]',
+                name: 'Name',
+                match: new RegExp('[a-zA-Z]','g'),
+                required: true,
+                error: function() {
+                    this.$element.closest('.form-group').addClass('has-error');
+                },
+                success: function() {
+                    this.$element.closest('.form-group').removeClass('has-error');
+                },
+            },
+            //email
+            {
+                selector: '[name="email"]',
+                name: 'Email',
+                required: true,
+                match: new RegExp("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$","g"),
+                error: function() {
+                    this.$element.closest('.form-group').addClass('has-error');
+                },
+                success: function() {
+                    this.$element.closest('.form-group').removeClass('has-error');
+                },
+            },
+            //password
+            {
+                selector: '[name="password"]',
+                name: 'Password',
+                required: true,
+                match: new RegExp("^(?=.*[a-z])(?=.*[0-9]).*$", "g"),
+                error: function() {
+                    this.$element.closest('.form-group').addClass('has-error');
+                },
+                success: function() {
+                    this.$element.closest('.form-group').removeClass('has-error');
+                },
+            },
+            //password2
+            {
+                selector: '[name="password2"]',
+                name: 'Password again',
+                required: true,
+                sameAs: '[name="password"]',
+                error: function() {
+                    this.$element.closest('.form-group').addClass('has-error');
+                },
+                success: function() {
+                    this.$element.closest('.form-group').removeClass('has-error');
+                },
+            }
+
+        ]);
+
+        //validate begins
+        $('.register-form').submit(function(){
+            if (validator.submit()) // all legal
+                return true;
+
+            //something illegal, output error messages
+            $('.register-alert').empty();
+            validator.errorMsgs.forEach(function(obj){
+
+                // customize error message
+                if (obj.name == 'Password again' && obj.attribute == 'sameAs') {
+                    obj.msg = obj.msg + ' as password';
+                }
+
+                var $err = $('<p class="text-danger">'+obj.msg+'</p>')
+                $('.register-alert').append($err);
+            })
+
+            $('.register-alert').removeClass('hidden');
+            return false;
+        }) //form submit
+
+    })
+
+
+
 });
 
 $(document).ready(function () {
@@ -209,3 +455,4 @@ $(function() {
         });
     }
 });
+
